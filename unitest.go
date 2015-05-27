4184d0f7677db50976dfa573b8bf431c421c0507
idada <bg5sbk@gmail.com>
2015-05-27 15:38:07 +0800

    分离进程监控代码

--- unitest.go
+++ unitest.go
@@ -7,10 +7,8 @@ import (
 	"os"
 	"regexp"
 	"runtime"
-	"runtime/pprof"
 	"strings"
 	"testing"
-	"time"
 )
 
 var (
@@ -19,19 +17,21 @@ var (
 )
 
 func Pass(t *testing.T, condition bool) bool {
-	if !condition {
-		log("[NOT PSSS]", passRegexp, "")
-		t.FailNow()
+	if condition {
+		return true
 	}
-	return condition
+	log("[NOT PSSS]", passRegexp, "")
+	t.FailNow()
+	return false
 }
 
 func NotError(t *testing.T, err error) bool {
-	if err != nil {
-		log("[ERROR]", notErrorRegexp, err.Error())
-		t.FailNow()
+	if err == nil {
+		return true
 	}
-	return err != nil
+	log("[ERROR]", notErrorRegexp, err.Error())
+	t.FailNow()
+	return false
 }
 
 func log(title string, regex *regexp.Regexp, val string) {
@@ -54,50 +54,4 @@ func log(title string, regex *regexp.Regexp, val string) {
 			}
 		}
 	}
-}
-
-// Command handler
-var CommandHandler func(string) bool
-
-func init() {
-	go func() {
-		for {
-			if input, err := ioutil.ReadFile("unitest.cmd"); err == nil && len(input) > 0 {
-				ioutil.WriteFile("unitest.cmd", []byte(""), 0744)
-
-				cmd := strings.Trim(string(input), " \n\r\t")
-
-				var (
-					profile  *pprof.Profile
-					filename string
-				)
-
-				switch cmd {
-				case "lookup goroutine":
-					profile = pprof.Lookup("goroutine")
-					filename = "unitest.goroutine"
-				case "lookup heap":
-					profile = pprof.Lookup("heap")
-					filename = "unitest.heap"
-				case "lookup threadcreate":
-					profile = pprof.Lookup("threadcreate")
-					filename = "unitest.thread"
-				default:
-					if CommandHandler == nil || !CommandHandler(cmd) {
-						println("unknow command: '" + cmd + "'")
-					}
-				}
-
-				if profile != nil {
-					file, err := os.Create(filename)
-					if err != nil {
-						println("couldn't create " + filename)
-					} else {
-						profile.WriteTo(file, 2)
-					}
-				}
-			}
-			time.Sleep(2 * time.Second)
-		}
-	}()
 }
